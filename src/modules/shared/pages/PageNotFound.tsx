e1754d4587c67fbf5db2e8dd93bd1d8ba6032e7f
Olga <olga.1.9.8.8.work@gmail.com>
2024-05-03 12:36:09 +0300

    add page not found

--- src/modules/shared/pages/PageNotFound.tsx
+++ src/modules/shared/pages/PageNotFound.tsx
@@ -1,5 +1,13 @@
+import { NotFound } from "../ui/icons";
+import { TypographyH5 } from "../ui/typography/TypographyH5";
+
 const PageNotFound = () => {
-  return <div>Не найдена страница</div>;
+  return (
+    <div className="flex flex-col items-center gap-6 p-6">
+      <NotFound className="h-80 w-80" />
+      <TypographyH5 className="text-center">Страница не найдена</TypographyH5>
+    </div>
+  );
 };
 
 // eslint-disable-next-line import/no-default-export
--- src/modules/shared/ui/icons/index.ts
+++ src/modules/shared/ui/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as LogoIcon } from "./logo.svg?react";
 export { default as LogoIconSmall } from "./logoSmall.svg?react";
 export { default as NoRoadIcon } from "./noRoad.svg?react";
+export { default as NotFound } from "./notFound.svg?react";
 export { default as SpinnerIcon } from "./spinner.svg?react";
