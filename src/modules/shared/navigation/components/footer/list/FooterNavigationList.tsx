9a50cf4661f75ac45467b3bdf9b08381029f480e
Olga <olga.1.9.8.8.work@gmail.com>
2024-10-31 11:31:18 +0300

    add uniq key for FooterNavigationListItem

--- src/modules/shared/navigation/components/footer/list/FooterNavigationList.tsx
+++ src/modules/shared/navigation/components/footer/list/FooterNavigationList.tsx
@@ -20,8 +20,9 @@ const footerItems = [
 export const FooterNavigationList = () => {
   return (
     <ul className="hidden flex-col text-sm leading-loose text-white lg:flex lg:flex-row xl:gap-2">
-      {footerItems.map((item) => (
-        <FooterNavigationListItem key={item.href} href={item.href} title={item.title}>
+      {footerItems.map((item, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <FooterNavigationListItem key={index} href={item.href} title={item.title}>
           {item.text}&#46;
         </FooterNavigationListItem>
       ))}
